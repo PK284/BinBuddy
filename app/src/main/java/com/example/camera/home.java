2bfc66c321b80455ad2d5ea8beb4cbf00031b613
Piyush Kumar <kumarpiyush2841@gmail.com>
2023-05-03 23:01:03 +0530

    new intent configured for some buttons

--- app/src/main/java/com/example/camera/home.java
+++ app/src/main/java/com/example/camera/home.java
@@ -1,12 +1,12 @@
 package com.example.camera;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 
 public class home extends AppCompatActivity {
 
-    ImageView profile, logout, cam;
+    ImageView profile, logout, cam,call,mail;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +26,8 @@ public class home extends AppCompatActivity {
         profile = findViewById(R.id.pro);
         logout = findViewById(R.id.logout);
         cam = findViewById(R.id.cam);
+        call = findViewById(R.id.call_btn);
+        mail = findViewById(R.id.mail);
 
         ImageSlider imageSlider = findViewById(R.id.image_slider);
         ArrayList<SlideModel> slideModels = new ArrayList<>();
@@ -54,5 +56,28 @@ public class home extends AppCompatActivity {
                 finish();
             }
         });
+
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(home.this,"Ok btn pressed",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:1929"));
+                startActivity(intent);
+            }
+        });
+
+        mail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/html");
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[] {"grievance@bmc.gov.in"});
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Garbage Complaint");
+                intent.putExtra(Intent.EXTRA_TEXT, "***Type your Complaint below**");
+                startActivity(Intent.createChooser(intent, "Send Email"));
+            }
+        });
+
     }
 }
